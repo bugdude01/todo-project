6ffb503219bfc4873e3e080b1f07fb2df9971003
Richard Rhodes <Richard.Rhodes@bf.local>
2020-04-27 12:45:58 +0100

    Tidy up and add homepage to test

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-def index(request):
-	return HttpResponse("Here's the tasks index")
+def home(request):
+	return render(request, 'tasks/home.html')
--- todo/urls.py
+++ todo/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+import tasks.views
+
 
 urlpatterns = [
-	path('tasks/', include('tasks.urls')),
+
     path('admin/', admin.site.urls),
+    path('', tasks.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
